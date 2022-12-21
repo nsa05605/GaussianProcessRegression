93940aada4ab2f85fb17ccf138b3aeed6d2a7f21
nsa05605 <nsa05605@naver.com>
2022-12-21 17:02:25 +0900

    2022. 12. 21

--- myGPR.py
+++ myGPR.py
@@ -25,7 +25,8 @@ print("Data Ready")
 poisson_likelihood = GPy.likelihoods.Poisson()
 laplace_inf = GPy.inference.latent_function_inference.Laplace()
 
-k1 = GPy.kern.RBF(input_dim=3, variance=0.3, lengthscale=0.2, ARD=False)
+#k1 = GPy.kern.RBF(input_dim=3, variance=0.8, lengthscale=0.5, ARD=False)
+k1 = GPy.kern.Matern32(input_dim=3, variance=0.8, lengthscale=0.5, ARD=False)
 k2 = GPy.kern.Bias(input_dim=3, variance=0.3)   # noise
 kernel = k1 + k2
 print("Kernel Initialized")
@@ -55,7 +56,10 @@ except cv2.error as ecv:
     print(ecv)
 
 print("Map Width (x) : ", map.shape[1], " , Map Height (y) : ", map.shape[0])
+# map.shape[1]은 x축 좌표, map.shape[0]는 y축 좌표를 의미
 
+# 아래는 metadata file(yaml file)을 불러옴
+# 여기에는 resolution, 시작 위치, occupied, free를 결정하는 threshold가 나와있음
 with open(directory + metaFile, 'r') as stream:
     try:
         metadata = yaml.safe_load(stream)
@@ -70,10 +74,15 @@ freespaceOnly = True;
 
 if freespaceOnly:
     cells = np.where(map == FREESPACE)
+    # freespace인 셀들을 찾고
     grid = np.array(list(zip(cells[1], (map.shape[0] - cells[0]))))
+    # list(zip([ ], [ ])) 하면 내부의 요소들끼리 [[1 2], [3 4], ... ] 의 배열로 결합된 형태로 생성됨
+    # 즉 여기에서의 grid는 [x, y] 로 이뤄진 값이 생성
     grid = grid*metadata["resolution"]
+    # resolution에 맞게 곱해줌
     grid[:, 0] = grid[:, 0] + metadata["origin"][0]
     grid[:, 1] = grid[:, 1] + metadata["origin"][1]
+    # 결과에 원점 위치를 더해줌
     grid[np.where((grid[:,0]<=2.5) & (grid[:, 1]<=0.0)), :]
 else:
     xnew = np.arange(0, map.shape[1])*metadata["resolution"] + metadata["origin"][0]
@@ -85,7 +94,20 @@ pred_point = grid
 print("Generated Grid of Points : " + str(grid.shape[0]))
 print("Sampling at Grid Points")
 
-print(pred_point)
+# print(pred_point)
+
+### sklearn이랑 결합해서 하는 방법이 있을지 생각
+### 없다면 sklearn으로 구현해야 할듯
+
+### 어쩌면 grid에 z축 좌표가 없어서 오류가 발생할 수도
+### grid 타입 확인해보고
+### z축 임의로 넣어서 결과 보기
+
+# print(pred_point)
+# print(len(pred_point))
+# print(pred_point.shape)
+# print(type(pred_point)) ## class 'numpy.ndarray
+#                         ## ndarray는 NumPy의 N차원 배열 객체
 
 ### 현재 p(기존의 pred_point)가 z축으로 하나의 값만 갖기 때문에 이를 확장할 필요가 있음
 ### resolution이 0.05이기 때문에, 0.0부터 1.0까지 0.05의 간격으로 z축을 만들 예정
@@ -142,45 +164,55 @@ for i in range(1, 21):
 print(p.shape)
 
 
-
-'''
 ### 3차원으로 확장하면 여기에서 차원 관련 오류
 ### 해결
 f_mean, f_var = m._raw_predict(p)
 f_mean = np.exp(f_mean)
 
 # print(f_mean)
-print(max(f_mean))
-print(min(f_mean))
+# print(max(f_mean))
+# print(min(f_mean))
+
+print(f_mean.shape)
+
+# print(max(f_mean))
+# print(np.where(f_mean == max(f_mean)))
+# print(f_mean[:,0][0])
+# print(f_mean[:,0][220948])
+
+### f_mean이 특정 임계값(ex_ 2?)보다 낮으면, 해당 지점을 pred_point에서 제거하는 코드
+
+p = np.delete(p, np.where(f_mean < 2), axis=0)
+f_mean = np.delete(f_mean, np.where(f_mean < 2), axis=0)
+# print(p.shape)
+# print(f_mean.shape)
 
 
-### sklearn이랑 결합해서 하는 방법이 있을지 생각
-### 없다면 sklearn으로 구현해야 할듯
 
-### 어쩌면 grid에 z축 좌표가 없어서 오류가 발생할 수도
-### grid 타입 확인해보고
-### z축 임의로 넣어서 결과 보기
 
-# print(pred_point)
-# print(len(pred_point))
-# print(pred_point.shape)
-# print(type(pred_point)) ## class 'numpy.ndarray
-#                         ## ndarray는 NumPy의 N차원 배열 객체
 
 xs = p[:, 0]    # x
 ys = p[:, 1]    # y
 zs = p[:, 2]    # z
 color = f_mean
 
+x = np.array(data_raw[:, 0])
+y = np.array(data_raw[:, 1])
+z = np.array(data_raw[:, 2])
+
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.scatter(xs, ys, zs, c=color, marker='s', s=1, cmap='jet')
-plt.xlim([-4,4])
-plt.ylim([-8,0])
+# x_values = np.arange(0, map.shape[1]) * metadata["resolution"] + metadata["origin"][0]
+# y_values = np.arange(0, map.shape[0]) * metadata["resolution"] + metadata["origin"][1]
+ax.scatter(xs, ys, zs, c=color, marker='s', s=1, cmap='jet', alpha=0.3)
+ax.scatter(x, y, z, c=Y, s=3, cmap='jet')
+ax.set_xlim([-5,3])
+ax.set_ylim([-8,0])
 ax.set_zlim([0,8])
 plt.show()
 
-fig2 = plt.figure(figsize=(6,10))
+'''
+fig2 = plt.figure(figsize=(6,6))
 ax2 = fig2.add_subplot(111, projection='3d')
 x = np.array(data_raw[:, 0])
 y = np.array(data_raw[:, 1])
